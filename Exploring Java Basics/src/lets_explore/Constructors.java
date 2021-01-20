4f31b956df78bb643ca9b82a406704118a286ff3
maggu <maggu@DESKTOP-U1BDCJC>
2021-01-21 00:15:55 +0530

    Defined Parameterized constructor

--- Exploring Java Basics/src/lets_explore/Constructors.java
+++ Exploring Java Basics/src/lets_explore/Constructors.java
@@ -24,12 +24,23 @@ class calc{
 	  Constructor have the name same as class name. 
 	  It's get called automatically, when the object is created.
 	  Constructor : Never return anything. {No return type}
+	  Constructors are of following types:
+	  	1. Default Constructors 		2. Parameterized Constructors 		3. Copy Constructors
 	*/
 	
-	public calc()					
+	public calc()								// Default Constructors 
 	{
 		num1 = 7;
 		num2 = 2;
+		System.out.println("In Default Constructor");
+	}
+	
+	public calc(int a,int b)					// Parameterized Constructors	
+	{
+		System.out.println("In Parameterizd Constructor");
+		num1 = a;
+		num2 = b;
+
 	}
 	
 }
@@ -38,9 +49,11 @@ public class Constructors {
 
 		public static void main(String args[])
 		{
-			calc c1 = new calc(); 		// c1 is the #object which is used as #reference to the class.
+			calc c1 = new calc();				// Default Constructor
 			c1.sum();
-			c1.sub();
+			calc c2 = new calc(15,9); 			// Parameterized Constructor
+			c2.sub();	
+			System.out.println("In Default Constructor"); 	// Since, C1 object is called, which is a default constructor.
 			c1.mul();
 			System.out.println("mul = " + c1.result);
 						
